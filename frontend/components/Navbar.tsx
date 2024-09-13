5fbf569574ba4e9848ad67d452a878a999f6600f
bobabob <sthienvanich@gmail.com>
2024-09-13 14:00:20 -0500

    fix navbar

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,5 +1,7 @@
+'use client';
+
 import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
+import { styled } from '@mui/system';
 import Box from '@mui/material/Box';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
@@ -17,12 +19,10 @@ const StyledToolbar = styled(Toolbar)(({ theme }) => ({
   alignItems: 'center',
   justifyContent: 'space-between',
   flexShrink: 0,
-  borderRadius: `calc(${theme.shape.borderRadius}px + 8px)`,
+  borderRadius: `calc(${theme.shape.borderRadius} + 8px)`,
   backdropFilter: 'blur(24px)',
   border: '1px solid',
   borderColor: theme.palette.divider,
-  backgroundColor: alpha(theme.palette.background.default, 0.4),
-  boxShadow: theme.shadows[1],
   padding: '8px 12px',
 }));
 
