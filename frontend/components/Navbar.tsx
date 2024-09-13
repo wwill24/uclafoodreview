9c25e86e7f13fe5a785131ffd6c31377221d7bc9
William MacBook <wwilliamw24@gmail.com>
2024-09-13 12:15:44 -0700

    small changes

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -13,6 +13,7 @@ import MenuItem from '@mui/material/MenuItem';
 import Drawer from '@mui/material/Drawer';
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
+import Image from 'next/image'
 
 const StyledToolbar = styled(Toolbar)(({ theme }) => ({
   display: 'flex',
