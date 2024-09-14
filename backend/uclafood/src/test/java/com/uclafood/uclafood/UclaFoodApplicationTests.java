ac1030396258495fbb099891f17034eb7f3f4e9f
William MacBook <wwilliamw24@gmail.com>
2024-09-14 15:27:50 -0700

    initialized spring boot

---
+++ backend/uclafood/src/test/java/com/uclafood/uclafood/UclaFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uclafood.uclafood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UclaFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
