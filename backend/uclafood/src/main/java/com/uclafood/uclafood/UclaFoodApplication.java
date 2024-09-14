c5660c73d0fa2600e625095709cf5f314ec6bde2
William MacBook <wwilliamw24@gmail.com>
2024-09-14 15:27:50 -0700

    initialized spring boot

---
+++ backend/uclafood/src/main/java/com/uclafood/uclafood/UclaFoodApplication.java
@@ -0,0 +1,13 @@
+package com.uclafood.uclafood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UclaFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UclaFoodApplication.class, args);
+	}
+
+}
