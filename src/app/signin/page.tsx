6592a01faca97b0c1113820b62583fde75171a67
William MacBook <wwilliamw24@gmail.com>
2024-09-14 23:18:05 -0700

    created sign in page

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,24 +1,22 @@
+'use client'
+
 import React from 'react';
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import Navbar from '../frontend/components/Navbar'
 
 export default function SignUp() {
   return (
     <div>
-      <h1>Sign Up</h1>
-      <form>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input type="text" id="username" name="username" required />
-        </div>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" required />
-        </div>
-        <div>
-          <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" required />
-        </div>
-        <button type="submit">Sign Up</button>
-      </form>
+      <Navbar />
+      <div className='flex flex-col items-center mt-[30vh]'>
+        <h1>Welcome Back!</h1>
+        <form className='flex flex-col items-center p-[2vh] gap-2'>
+          <Input minLength={5} className='' placeholder='Username'/>
+          <Input minLength={8} className='' placeholder='Password'/>
+          <Button className='items-center p-[1vh] w-45/100 h-1/2' variant="outline">Log In</Button>
+        </form>
+      </div>
     </div>
   );
 }
