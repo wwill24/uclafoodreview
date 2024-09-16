fa4e07c9d872d44f5bcb1b6c3f7d3645187fb04d
bobabob <sthienvanich@gmail.com>
2024-09-16 13:55:11 -0500

    created and implemented a resuableform

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,35 +1,10 @@
 'use client'
 
 import React from 'react';
-import Link from 'next/link';
 
 // Form
 import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form"
-
-// Shadcn
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage
-} from "@/components/ui/form";
-
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle
-} from "@/components/ui/card";
-
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
+import ReusableForm from '../../../../frontend/components/ResusableForm';
 
 const formSchema = z.object({
   username: z.string().min(1, "Please enter a username"),
@@ -37,14 +12,6 @@ const formSchema = z.object({
 })
 
 export default function SignIn() {
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      username: "",
-      password: "",
-    },
-  });
-
   async function onSubmit(values: z.infer<typeof formSchema>) {
     try {
       const res = await fetch("http://localhost:8080/signin", { method: "POST", headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(values) });
@@ -56,52 +23,16 @@ export default function SignIn() {
   }
 
   return (
-    <div className='flex flex-col justify-center items-center h-screen'>
-      <Card className='p-4 w-[50vh]'>
-        <CardHeader className='flex flex-col gap-2 text-center'>
-          <CardTitle>Welcome Back!</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <Form {...form}>
-            <form className='flex flex-col items-center space-y-2' onSubmit={form.handleSubmit(onSubmit)}>
-              {/* Username */}
-              <FormField
-                control={form.control}
-                name="username" 
-                render={({ field }) => (
-                  <FormItem className='w-full'>
-                    <FormControl>
-                      <Input placeholder="Username" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              
-              {/* Password */}
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem className='w-full'>
-                    <FormControl>
-                      <Input placeholder="Password" type="password" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              {/* Submit Button */}
-              <Button className='w-full h-1/2 bg-[#007ec4] hover:bg-[#00a6ff] text-[#fff] hover:text-[#fff]' variant="ghost" type="submit">Sign In</Button>
-            </form>
-          </Form>
-          <span className="inline-flex gap-1 mt-2">
-            <Link href="/auth/forgotpassword">Forgot Password?</Link>
-          </span>
-        </CardContent>
-      </Card>
-    </div>
+    <ReusableForm
+      schema={formSchema}
+      formInputs={[
+        { name: 'username', label: 'Username', placeholder: 'Username' },
+        { name: 'password', label: 'Password', placeholder: 'Password', type: 'password' }
+      ]}
+      onSubmit={onSubmit}
+      cardTitle="Welcome Back!"
+      submitButtonText="Sign In"
+    />
   );
   
 }
