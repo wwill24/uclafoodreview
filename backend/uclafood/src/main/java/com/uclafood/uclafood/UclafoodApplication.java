be5134e1349beb3b5660e949c7f444600e08debe
bobabob <sthienvanich@gmail.com>
2024-09-16 01:19:18 -0500

    connect frontend form to backend

--- backend/uclafood/src/main/java/com/uclafood/uclafood/UclafoodApplication.java
+++ backend/uclafood/src/main/java/com/uclafood/uclafood/UclafoodApplication.java
@@ -2,20 +2,32 @@ package com.uclafood.uclafood;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.logging.Logger;
 
 @SpringBootApplication
 @RestController
 public class UclafoodApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UclafoodApplication.class, args);
-	}
+    private static final Logger logger = Logger.getLogger(UclafoodApplication.class.getName());
 
-	@GetMapping("/")
-	public String home() {
-		return "Welcome to UCLA Food!";
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UclafoodApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to UCLA Food!";
+    }
+
+	// TODO: add prod server URL
+	@CrossOrigin(origins = "http://localhost:3000")
+    @PostMapping("/signup")
+    public String signup(@RequestBody Map<String, Object> payload) {
+        logger.info("Received signup data: " + payload.toString());
+
+        return "Signup data received successfully!";
+    }
 }
