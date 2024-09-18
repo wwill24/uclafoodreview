6208a9e53011e5bd708b0a2d955c2f70add5eb6e
bobabob <sthienvanich@gmail.com>
2024-09-17 19:44:53 -0500

    moved user signin and signup endpoints to UserController

--- backend/uclafood/src/main/java/com/uclafood/uclafood/UclafoodApplication.java
+++ backend/uclafood/src/main/java/com/uclafood/uclafood/UclafoodApplication.java
@@ -4,36 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
-import java.util.logging.Logger;
-
 @SpringBootApplication
 @RestController
-@CrossOrigin
 public class UclafoodApplication {
-
-    private static final Logger logger = Logger.getLogger(UclafoodApplication.class.getName());
 
     public static void main(String[] args) {
         SpringApplication.run(UclafoodApplication.class, args);
     }
 
-    @GetMapping("/")
-    public String home() {
-        return "Welcome to UCLA Food!";
-    }
-
-    @PostMapping("/signup")
-    public String signup(@RequestBody Map<String, Object> payload) {
-        logger.info("Received signup data: " + payload.toString());
-
-        return "Signup data received successfully!";
-    }
-
-    @PostMapping("/signin")
-    public String signin(@RequestBody Map<String, Object> payload) {
-        logger.info(payload.toString());
-
-        return "Signin data received successfully!";
-    }
 }
---
+++ backend/uclafood/src/main/java/com/uclafood/uclafood/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.uclafood.uclafood.controller;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.uclafood.uclafood.UclafoodApplication;
+import java.util.logging.Logger;
+
+@RestController
+@CrossOrigin
+public class UserController {
+    
+    private static final Logger logger = Logger.getLogger(UclafoodApplication.class.getName());
+    
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to UCLA Food!";
+    }
+
+    @PostMapping("/signup")
+    public String signup(@RequestBody Map<String, Object> payload) {
+        logger.info("Received signup data: " + payload.toString());
+
+        return "Signup data received successfully!";
+    }
+
+    @PostMapping("/signin")
+    public String signin(@RequestBody Map<String, Object> payload) {
+        logger.info(payload.toString());
+
+        return "Signin data received successfully!";
+    }
+
+}
