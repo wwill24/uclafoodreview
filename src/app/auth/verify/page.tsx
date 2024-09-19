6eebd0ee2cb46e6d213616236c6b487ef8f04404
bobabob <sthienvanich@gmail.com>
2024-09-19 16:13:42 -0500

    fix git conflicts

--- src/app/auth/verify/page.tsx
+++ src/app/auth/verify/page.tsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 'use client';
-=======
-'use client'
->>>>>>> 093928bc75daf066872309fa508ad388e74bb002
 
 import { useState, useEffect } from "react";
 
@@ -50,29 +46,9 @@ export default function Verification({ otp, otpGenerated }: props) {
           Verify Using The OTP Sent To Your Email
         </CardHeader>
         <CardContent className="flex flex-col items-center gap-4">
-<<<<<<< HEAD
           <OTP />
           <Button onClick={() => {verify}} className='w-full h-1/2 bg-[#007ec4] hover:bg-[#00a6ff] text-[#fff] hover:text-[#fff]' variant="ghost" type="submit">Verify</Button>
         </CardContent>
-=======
-          <InputOTP maxLength={6} className="w-[15vw]">
-            <InputOTPGroup>
-              <InputOTPSlot 
-                index={0} 
-              />
-              <InputOTPSlot index={1} />
-              <InputOTPSlot index={2} />
-            </InputOTPGroup>
-            <InputOTPSeparator />
-            <InputOTPGroup>
-              <InputOTPSlot index={3} />
-              <InputOTPSlot index={4} />
-              <InputOTPSlot index={5} />
-            </InputOTPGroup>
-          </InputOTP>
-          <Button onClick={() => {verify}} className='w-[15vw] h-1/2 bg-[#007ec4] hover:bg-[#00a6ff] text-[#fff] hover:text-[#fff]' variant="ghost" type="submit">Verify</Button>
-        </CardContent>  
->>>>>>> 093928bc75daf066872309fa508ad388e74bb002
       </Card>
     </div>
   );
