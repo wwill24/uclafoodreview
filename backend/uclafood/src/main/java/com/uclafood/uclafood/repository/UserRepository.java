d6f6277ca4626fdae52f65f7af412ab375f5f030
bobabob <sthienvanich@gmail.com>
2024-09-19 13:37:48 -0500

    created existsByPhone & existsByUsername

--- backend/uclafood/src/main/java/com/uclafood/uclafood/repository/UserRepository.java
+++ backend/uclafood/src/main/java/com/uclafood/uclafood/repository/UserRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
+    boolean existsByPhone(String phone);
+    boolean existsByUsername(String username);
 }
