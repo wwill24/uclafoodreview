debe59c0cf8472af237a556000eac5760df44ad1
William MacBook <wwilliamw24@gmail.com>
2024-09-19 13:19:32 -0700

    creating otp

--- src/app/auth/verify/page.tsx
+++ src/app/auth/verify/page.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 import {
     InputOTP,
     InputOTPGroup,
@@ -5,23 +7,47 @@ import {
     InputOTPSlot,
   } from "@/components/ui/input-otp"
 
-  import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle
-  } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from "@/components/ui/card";
 
-export default function Verify() {
-    return (
-      <Card className="flex flex-col items-center justify-center">
-        <CardHeader>
+import { Button } from "@/components/ui/button";
+
+export default function Verification() {
+  const [otp, setOtp] = useState(Array(6).fill(""));
+  const [otpGenerated, setOtpGenerator] = useState<string>("");
+
+  const otpGenerator = () => {
+    const otp = Math.floor(100000 + Math.random() * 900000);
+    setOtpGenerator(otpGenerated);
+    console.log(otp);
+  }
+
+  useEffect(() => {
+    otpGenerator();
+  }, [])
+
+  const handleInput = () => {
+    
+  }
+
+  async function verify(){
+    
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <Card className="p-4 w-[50vh]">
+        <CardHeader className="text-center">
           Verify Using The OTP Sent To Your Email
         </CardHeader>
-        <CardContent>
-          <InputOTP maxLength={6}>
+        <CardContent className="flex flex-col items-center gap-4">
+          <InputOTP maxLength={6} className="w-full">
             <InputOTPGroup>
               <InputOTPSlot index={0} />
               <InputOTPSlot index={1} />
@@ -34,8 +60,11 @@ export default function Verify() {
               <InputOTPSlot index={5} />
             </InputOTPGroup>
           </InputOTP>
-        </CardContent>
+          <Button onClick={() => {verify}} className='w-[20vw] h-1/2 bg-[#007ec4] hover:bg-[#00a6ff] text-[#fff] hover:text-[#fff]' variant="ghost" type="submit">Verify</Button>
+        </CardContent>  
       </Card>
-    );
+    </div>
+
+  );
 }
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,15 @@ const config: Config = {
   ],
   theme: {
   	extend: {
+		keyframes: {
+			"caret-blink": {
+				"0%,70%,100%": { opacity: "1" },
+				"20%,50%": { opacity: "0" },
+			},
+		},
+		animation: {
+			"caret-blink": "caret-blink 1.25s ease-out infinite",
+		},
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
@@ -58,16 +67,7 @@ const config: Config = {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
-  		},
-		keyframes: {
-		"caret-blink": {
-			"0%,70%,100%": { opacity: "1" },
-			"20%,50%": { opacity: "0" },
-		},
-		},
-		animation: {
-		"caret-blink": "caret-blink 1.25s ease-out infinite",
-		},
+  		}
   	}
   },
   plugins: [require("tailwindcss-animate")],
