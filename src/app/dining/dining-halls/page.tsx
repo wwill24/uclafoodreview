442b69d9c1412f847025e6d8c712e62438a9bfc3
bobabob <sthienvanich@gmail.com>
2024-09-23 14:57:37 -0500

    new business card

--- src/app/dining/dining-halls/page.tsx
+++ src/app/dining/dining-halls/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import BusinessCard from "@/components/Business"
+import BusinessCard from "@/components/BusinessCard"
 import ReviewForm from "@/components/ReviewForm"
 
 import { useState } from 'react';
@@ -8,7 +8,8 @@ import { useState } from 'react';
 export default function DiningHalls() {
   const [signUpFormData, setSignUpFormData] = useState({});
   return (
-    <div>
+    <div className="flex flex-col gap-4 m-[2em]">
+      <BusinessCard />
       <BusinessCard />
     </div>
   )
---
+++ src/components/BusinessCard.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import React from 'react';
+import Link from 'next/link';
+import { Star } from 'lucide-react';
+import Image from 'next/image';
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
+import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation';
+import epicimg from '../../frontend/img/epicuriaimg.png';
+
+import { useState } from 'react';
+
+export default function BusinessCard() {
+  const router = useRouter();
+  const name = 'Epicuria';
+  const starRating = 4.5;
+  const reviewsCount = 12;
+
+  return (
+    <Card className="w-[40vw] h-[20vh] flex flex-row p-4 gap-4 items-center shadow-lg hover:shadow-2xl">
+      <div>
+        <Image src={epicimg} height={120} width={120} alt="epicuria image" className="rounded-md" />
+      </div>
+      <div className='flex items-center'>
+        <div className='w-full h-full'>
+          <CardTitle className="text-xl text-[#238dd3]">{name}</CardTitle>
+          <div className="flex items-center gap-0.5">
+            {Array.from({ length: 5 }).map((_, i) => (
+              <Star
+                key={i}
+                className={`${i < Math.floor(starRating) ? 'text-[#2b6db8]' : 'text-gray-300'}`}
+              />
+            ))}
+            <span className="text-gray-600 text-sm">{starRating} ({reviewsCount} reviews)</span>
+          </div>
+          <CardDescription className="text-gray-500 text-sm mt">PIZZA PIZZA SO YUMMY!</CardDescription>
+          <div className='flex flex-row items-end justify-end'>
+            <Button className="mt-2 bg-[#4773ec] text-white rounded-md text-sm" onClick={() => router.push('/review/epicuria')}>Leave Review</Button>
+          </div>
+        </div>
+      </div>
+    </Card>
+  );
+}
