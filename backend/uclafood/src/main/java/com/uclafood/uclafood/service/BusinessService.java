4a6245d4fa84f0eaaff185e65be35cc63d40bfdf
William MacBook <wwilliamw24@gmail.com>
2024-09-23 12:57:35 -0700

    minor changes

--- backend/uclafood/src/main/java/com/uclafood/uclafood/service/BusinessService.java
+++ backend/uclafood/src/main/java/com/uclafood/uclafood/service/BusinessService.java
@@ -1,7 +1,6 @@
 package com.uclafood.uclafood.service;
 
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
--- src/app/dining/dining-halls/page.tsx
+++ src/app/dining/dining-halls/page.tsx
@@ -1,12 +1,33 @@
 'use client'
 
 import BusinessCard from "@/components/Business"
-import ReviewForm from "@/components/ReviewForm"
 
-import { useState } from 'react';
+import toast, { Toaster } from "react-hot-toast";
+
+interface Business{
+  businessName: string,
+  address: string,
+  rating: number
+}
 
 export default function DiningHalls() {
-  const [signUpFormData, setSignUpFormData] = useState({});
+  async function getBusinesses() {
+    try {
+      const getBusinessesReq = await fetch ("http://localhost:8080/getBusiness", {
+        method: "GET",
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      const businessData = await getBusinessesReq.json();
+      console.log(businessData);
+    }
+    catch (err: any) {
+      console.error(err);
+      toast.error("Could not retrieve businesses");
+    }
+  }
+
   return (
     <div>
       <BusinessCard />
