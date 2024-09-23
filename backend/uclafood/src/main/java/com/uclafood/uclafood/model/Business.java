7f3f7e0e4876e6a49b55df327bdc327d9207b27a
William MacBook <wwilliamw24@gmail.com>
2024-09-23 12:09:45 -0700

    created business api

---
+++ backend/uclafood/src/main/java/com/uclafood/uclafood/model/Business.java
@@ -0,0 +1,63 @@
+package com.uclafood.uclafood.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "business")
+public class Business {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="businessname", nullable = false)
+    private String businessName;
+
+    @Column(name="category", nullable = false)
+    private String category;
+
+    @Column(name="address", nullable = false)
+    private String address;
+
+    @Column(name="rating", nullable = false)
+    private Float rating;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBusinessName() {
+        return businessName;
+    }
+
+    public void setBusinessName(String businessName) {
+        this.businessName = businessName;
+    }
+
+    public String getcategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Float getRating() {
+        return rating;
+    }
+
+    public void setRating(Float rating) {
+        this.rating = rating;
+    }
+}
