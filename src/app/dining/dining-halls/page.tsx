3c1ddb2d0639d1946bbffabc3e99fe84a77ed3fb
bobabob <sthienvanich@gmail.com>
2024-09-24 00:22:29 -0500

    added 3 loading skeletons

--- src/app/dining/dining-halls/page.tsx
+++ src/app/dining/dining-halls/page.tsx
@@ -38,7 +38,9 @@ export default function DiningHalls() {
     <div className="flex flex-col gap-4 m-[2em] items-center">
       {businessData ? businessData.map((data: any, index: number) => (
         <BusinessCard key={index} businessName={data.businessName} address={data.address} rating={data.rating} description={data.description}/>
-      )) : <BusinessCardSkeleton />}
+      )) : Array.from({ length: 3 }).map((_, i) => ( <BusinessCardSkeleton /> ))}
+      {/* <BusinessCardSkeleton /> */}
+      <Toaster position="bottom-right" />
     </div>
   )
 }
--- src/components/BusinessCardSkeleton.tsx
+++ src/components/BusinessCardSkeleton.tsx
@@ -1,31 +1,20 @@
-import { Star } from 'lucide-react';
-import Image from 'next/image';
-
-import {
-  Card,
-  CardDescription,
-  CardTitle
-} from '@/components/ui/card';
-import { Button } from '@/components/ui/button';
+import { Card } from '@/components/ui/card';
 import { Skeleton } from "@/components/ui/skeleton";
 
 export default function BusinessCardSkeleton() {
   return (
     <Card className="w-[40vw] h-[20vh] flex flex-row p-4 gap-4 items-center shadow-lg">
-      <Skeleton className="w-[120px] h-[120px] rounded-md" />
+      <Skeleton className="w-28 h-28 rounded-md" />
       <div className='flex items-center'>
         <div className='w-[27vw] h-full'>
-          <Skeleton className="w-[15vw] h-[24px] mb-2" />
-          <div className="flex items-center gap-0.5 mb-2">
-            {Array.from({ length: 5 }).map((_, i) => (
-              <Skeleton key={i} className="w-5 h-5 rounded" />
-            ))}
-            <Skeleton className="w-[8vw] h-[16px]" />
+          <Skeleton className="w-[15vw] h-6 mb-2" />
+          <div className="flex items-center gap-1 mb-2">
+            <Skeleton className="w-[8vw] h-4" />
           </div>
-          <Skeleton className="w-[100%] h-[16px] mb-1" />
-          <Skeleton className="w-[100%] h-[16px] mb-1" />
+          <Skeleton className="w-full h-4 mb-1" />
+          <Skeleton className="w-full h-4 mb-1" />
           <div className='flex flex-row items-end justify-end'>
-            <Skeleton className="w-[8vw] h-[32px] rounded-md mt-2" />
+            <Skeleton className="w-[8vw] h-8 rounded-md mt-2" />
           </div>
         </div>
       </div>
