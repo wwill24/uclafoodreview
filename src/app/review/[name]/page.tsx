22bdff943f275cf58834531911b7ff77958ea82c
bobabob <sthienvanich@gmail.com>
2024-09-28 21:52:37 -0500

    fix review form no work now work :)

--- src/app/review/[name]/page.tsx
+++ src/app/review/[name]/page.tsx
@@ -4,25 +4,49 @@ import ReviewForm from '@/components/ReviewForm'
 import { useState, useEffect } from 'react';
 import { useParams, useRouter } from 'next/navigation';
 
-
 import toast, { Toaster } from "react-hot-toast";
 
+import { removeNonAlphabetic } from "@/lib/utils";
+
 interface BusinessData {
-  businessName: string,
-  address: string,
-  rating: number,
-  description: string
+    id: number,
+    businessName: string,
+    address: string,
+    rating: number,
+    description: string
 }
 
-export default function Review(){
-    const [signUpFormData, setSignUpFormData] = useState({});
-    const router = useRouter();
+export default function Review() {
     const params = useParams();
+
+    const [businessID, setBusinessID] = useState<number>(0);
+
     const businessName = params.name.toString();
 
+    useEffect(() => {
+        (async () => {
+            try {
+                const getBusinessIDReq = await fetch(`http://localhost:8080/getBusiness?businessName=${removeNonAlphabetic(businessName)}`, {
+                    method: "GET",
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                })
+                const req: BusinessData = await getBusinessIDReq.json();
+                setBusinessID(req.id);
+            }
+            catch (e) {
+                console.error(e);
+                toast.error("Something went wrong. Please try again later.");
+            }
+        })();
+    }, []);
+
     return (
-        <div>
-            <ReviewForm name={businessName} setFormData={setSignUpFormData} />
-        </div>
+        <>
+            {businessID == 0 ? "" :
+                <ReviewForm name={businessName} businessID={businessID} />
+            }
+        </>
     )
 }
